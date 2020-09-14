00aa2a926f8e7be63b696dc3eb293b9170b7f59d
Илья <stive-abnett@yandex.ru>
2020-09-14 19:40:24 +0700

    First Code

---
+++ FinBack/Models/Balance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinBack.Models
+{
+    public class Balance
+    {
+        public int Id { get; set; }
+        public string AccountName { get; set; }
+        public int Amount { get; set; }
+
+        public int ClientId { get; set; }
+        public Client Client { get; set; }
+    }
+}
---
+++ FinBack/Models/Context.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinBack.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Balance> Balances { get; set; }
+        
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+      
+        public static void Initialize(Context db)
+        {
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+            Client c1 = new Client { Name = "Ilya", Surname = "Martynenko", MiddleName = "Gennadievich", BirthDate = new DateTime(1995, 12, 17) };
+
+            db.Clients.AddRange(new List<Client> { c1 });
+            db.SaveChanges();
+
+            Balance b1 = new Balance { AccountName = "Broker", Amount = 1000, ClientId = c1.Id };
+            db.Balances.AddRange(new List<Balance> { b1 });
+            db.SaveChanges();
+        }
+        /*
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=db;Trusted_Connection=True;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+        }*/
+    }
+}
